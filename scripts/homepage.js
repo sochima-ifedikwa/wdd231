d3bc57b0e08eaf7653345d9be26765b4d0b4d3d7
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-03-22 17:58:12 -0500

    `Refactor index.html, remove katherine.html, and update styles/large.css and styles/smalls.css`

---
+++ scripts/homepage.js
@@ -0,0 +1,8 @@
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('open');
+    hambutton.classList.toggle('open');
+    
+});
