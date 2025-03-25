d23baab4cb2aefd34e87fbb7bc59e556e6fbc4af
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-03-25 14:18:02 -0500

    made edits to js

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,8 +1,13 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `${d.getFullYear()}`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('open');
     hambutton.classList.toggle('open');
-    
 });
+
+date.textContent = date.las
