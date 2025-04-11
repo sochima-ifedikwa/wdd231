d0b4af40d0e9caca2a09d4e6c7a17e942a8e90f6
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-04-11 14:32:07 -0500

    `Refactored discover.js and updated discover.html to use new discover.js file, removed discover copy.js file`

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,58 +1,52 @@
-// discover.js
-
-// Get the reference to the division where we display the items
 const showHere = document.querySelector("#allplaces");
-const places = './data/places.json';
 
-// Fetch the JSON data from the file
-fetch(places)
-  .then(response => response.json())
-  .then(data => {
-    // Loop through the array of JSON items
-    function displayItems(places) {
-      places.forEach(x => {
-        // Build the card element
-        const thecard = document.createElement('div');
-        // Build the photo element
-        const thephoto = document.createElement('img');
-        thephoto.src = `images/${x.photo_link}`;
-        thephoto.alt = x.name;
-        thecard.appendChild(thephoto);
-        // Build the title element
-        const thetitle = document.createElement('h2');
-        thetitle.innerText = x.name;
-        thecard.appendChild(thetitle);
-        // Build the address element
-        const theaddress = document.createElement('address');
-        theaddress.innerText = x.address;
-        thecard.appendChild(theaddress);
-        // Build the description element
-        const thedesc = document.createElement('p');
-        thedesc.innerHTML = `${x.description} <br><br>COST:  ${x.cost}`;
-        thecard.appendChild(thedesc);
-        // Build the button
-        const thelearn = document.createElement('button');
-        thelearn.innerText = "Learn More";
-        thecard.appendChild(thelearn);
+const path = './data/places.json';
 
-        // Add an event listener to the button to show more information
-        thelearn.addEventListener('click', () => {
-          showStuff(x);
-        });
+async function getPlaces () {
+  try { 
+    const response = await fetch(path);
+    const data = await response.json();
+    // console.log(data.places);
+    displayPlaces(data.places);
+  } catch (error) {
+    console.error('Error fetching places:', error);
+  }
+}
 
-        showHere.appendChild(thecard);
-      }); // end loop
-    } // end function
+getPlaces();
 
-    // Call the displayItems function with the fetched data
-    displayItems(data);
+const displayPlaces = (allPlaces) => {
+  allPlaces.forEach((place) => {
+    const thecard = document.createElement('div')
+    //build the photo element
+    const thephoto = document.createElement('img')
+    thephoto.src = `images/${place.photo_link}`
+    thephoto.alt = place.name
+    thecard.appendChild(thephoto)
+    //build the title element
+    const thetitle = document.createElement('h2')
+    thetitle.innerText = place.name
+    thecard.appendChild(thetitle)
+    //build the address element
+    const theaddress = document.createElement('address')
+    theaddress.innerText = place.address
+    thecard.appendChild(theaddress)
+    //build the description element
+    const thedesc = document.createElement('p')
+    thedesc.innerHTML = `${place.description} <br><br>COST:  ${place.cost}`
+    thecard.appendChild(thedesc)
+    //build the button
+    const thelearn = document.createElement('button')
+    thelearn.innerText = "Learn More"
+    thecard.appendChild(thelearn)
+
+
+    showHere.appendChild(thecard)
   })
-  .catch(error => console.error('Error fetching data:', error));
+}
 
-// Populate the dialog with information when the button is clicked
-function showStuff(x) {
-  // Assuming mytitle, myinfo, and mydialog are defined elsewhere in the code
-  mytitle.innerHTML = x.name;
-  myinfo.innerHTML = `Dedicated ${x.dedicated} by ${x.person} as temple number ${x.number}`;
+function showStuff(place) {
+  mytitle.innerHTML = x.name
+  myinfo.innerHTML = `Dedicated ${place.dedicated} by ${place.person} as temple number ${place.number}`
   mydialog.showModal();
-} // end of function
+}
